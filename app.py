d5efa7f2a5841e482814da6a56d9c8b99b879246
Murali Allada <amuralis@hotmail.com>
2015-11-14 19:21:01 -0600

    pep8 fixes

--- app.py
+++ app.py
@@ -1,23 +1,18 @@
 #!/usr/bin/env python
-
-import sys, traceback
-sys.path.append("./py-wink")
-
-import wink
-
 import ConfigParser
-import time
-import json
 import datetime
-import mintapi
+import sys
+import time
+import traceback
+import json
+
 from googlefinance import getQuotes
+import mintapi
 from yahoo_finance import Share
 
-def scale_value(x, in_min, in_max, out_min, out_max):
-    try:
-        return (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
-    except ZeroDivisionError:
-        return 0
+sys.path.append("./py-wink")
+import wink
+
 
 def percentage(part, whole):
     if part < 1:
@@ -48,50 +43,49 @@ class Nimbus(object):
         self.__dict__ = c.__dict__
 
     def set_dial_value(self, dial_num, percent, label):
-            dial = self.dials()[dial_num]
-            # the dial servo will always display a percentage [0..100],
-            # we'll set up the dial minimum and maximum to reflect that:
-            dial_config = {
-                "scale_type": "linear",
-                "rotation": "cw",
-                "min_value": 0,
-                "max_value": 100,
-                "min_position": 0,
-                "max_position": 360,
-                "num_ticks": 12
-            }
+        dial = self.dials()[dial_num]
+        # the dial servo will always display a percentage [0..100],
+        # we'll set up the dial minimum and maximum to reflect that:
+        dial_config = {
+            "scale_type": "linear",
+            "rotation": "cw",
+            "min_value": 0,
+            "max_value": 100,
+            "min_position": 0,
+            "max_position": 360,
+            "num_ticks": 12
+        }
 
-            # log statement:
-            current_time = datetime.datetime.now().time()
-            print "percent = %d%%, label = %s" % (percent, label)
+        # log statement:
+        current_time = datetime.datetime.now().time()
+        print "percent = %d%%, label = %s" % (percent, label)
 
-            # assert manual control (chan. 10) with new config, value, & label:
-            dial.update(dict(
-                channel_configuration=dict(channel_id="10"),
-                dial_configuration=dial_config,
-                label=label,
-                value=percent,
-            ))
+        # assert manual control (chan. 10) with new config, value, & label:
+        dial.update(dict(
+            channel_configuration=dict(channel_id="10"),
+            dial_configuration=dial_config,
+            label=label,
+            value=percent,
+        ))
 
 
 def main():
-    app_cfg = ConfigParser.RawConfigParser()
-    app_cfg.read("./cfg/app.cfg")
-    update_period_sec = int(app_cfg.get('global', 'update_period_sec'))
+    cfg = ConfigParser.RawConfigParser()
+    cfg.read("./cfg/app.cfg")
+    update_period_sec = int(cfg.get('global', 'update_period_sec'))
 
     my_nimbus = Nimbus("./cfg/wink.cfg")
 
-    mint_cfg = ConfigParser.RawConfigParser()
-    mint_cfg.read("./cfg/mint.cfg")
-    mint_email = mint_cfg.get('auth', 'email')
-    mint_password = mint_cfg.get('auth', 'password')
-    monthly_budget = mint_cfg.get('auth', 'monthly_budget')
+    cfg.read("./cfg/mint.cfg")
+    mint_email = cfg.get('auth', 'email')
+    mint_password = cfg.get('auth', 'password')
+    monthly_budget = cfg.get('setting', 'monthly_budget')
 
     while 1:
         # stocks
         oas = Share('OAS')
         oas_open_price = float(oas.get_open())
-        oas_json = json.dumps(getQuotes('OAS'), indent = 2)
+        oas_json = json.dumps(getQuotes('OAS'), indent=2)
         oas_data = json.loads(oas_json)[0]
         oas_price = float(oas_data['LastTradePrice'])
         percent = percentage(oas_price, oas_open_price)
@@ -100,7 +94,7 @@ def main():
 
         ugaz = Share('UGAZ')
         ugaz_open_price = float(ugaz.get_open())
-        ugaz_json = json.dumps(getQuotes('UGAZ'), indent = 2)
+        ugaz_json = json.dumps(getQuotes('UGAZ'), indent=2)
         ugaz_data = json.loads(ugaz_json)[0]
         ugaz_price = float(ugaz_data['LastTradePrice'])
         percent = percentage(ugaz_price, ugaz_open_price)
@@ -109,7 +103,7 @@ def main():
 
         # monthly budget from mint
         mint = mintapi.Mint(mint_email, mint_password)
-        budgets = json.loads(json.dumps(mint.get_budgets(), indent = 2))
+        budgets = json.loads(json.dumps(mint.get_budgets(), indent=2))
         total_spent = 0
         can_spend = 0
         for budget in budgets['spend']:
@@ -134,7 +128,7 @@ if __name__ == "__main__":
         except KeyboardInterrupt:
             ret = 0
             break
-        except:
+        except ex:
             print "Exception:"
             print '-'*60
             traceback.print_exc(file=sys.stdout)
